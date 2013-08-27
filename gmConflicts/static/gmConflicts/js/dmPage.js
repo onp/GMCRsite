8be4b3cf95877de3b510eba8b16f26f605adb185
Oskar Petersons <a.more.reliable.source@gmail.com>
2013-08-26 23:13:48 -0400

    create javascript object model backend for editor
    major changes to a large number of files, implementing object models for
    decision makers, options, and the overall conflict.  Link instances
    multiple instances of options together for simultaneous updating.

--- gmConflicts/static/gmConflicts/js/dmPage.js
+++ gmConflicts/static/gmConflicts/js/dmPage.js
@@ -22,16 +22,159 @@ var dmPageScript = function(){
 							"water.png",
 	]};	
     
-	var newOpt = {optName:	"new option",
-				  image:	iconList.icons[0]  };
-	
-    var newDM = {
-		name: "new decision maker",
-		options: [	newOpt	]
-	};	
+    function OptionObj(conflict,optionData){
+        if (typeof optionData === "undefined"){
+            optionData = {name:	"new option",
+                          image:    iconList.icons[0],  
+                          reversible: "Reversible"};
+        };
+        var _conf = conflict;
+        var _opt = this;
+        this.name = optionData.name;
+        this.image = optionData.image;
+        this.reversible = optionData.reversible;
+        this.views = [];
+        
+        this.toJSON = function(){
+            return {"name":this.name,"image":this.image,"reversible":this.reversible}
+        }
+        
+        this.updateViews = function(){
+            $.each(this.views,function(i){
+                this.find('.rev').val(_opt.reversible);
+                this.find('.optName').val(_opt.name);
+                this.children('.icon').attr("src","/static/gmConflicts/images/option_icons/" + _opt.image);
+            });
+        };
+        
+        this.renderOption = function(){
+            $newView = $(optMaker(_opt));
+            $newView.find('.rev').val(this.reversible).change(function(){
+                _opt.reversible = $(this).val();
+                _opt.updateViews();
+            });
+            $newView.find('.optName').change(function(){
+                _opt.name = $(this).val();
+                _opt.updateViews();
+            });
+            $newView.children('.icon').change(function(){
+                _opt.image = $(this).attr("src")
+                                   .match(/images\/option\_icons\/([.\w]+)/)[1];
+                _opt.updateViews();
+            });
+            this.views.push($newView);
+            return $newView
+        }
+    }
+    
+    function DMObj(conflict,dmData){
+        if (typeof dmData === "undefined"){
+            dmData = {name:	"new Decision maker",
+                      options: []};
+        };
+        var _conf = conflict;
+        var _dm = this;
+        this.name = dmData.name;
+        this.options = $.map(dmData.options,function(opt){
+            return _conf.options[opt]
+        });
+        
+        this.renderDM = function(){
+            $dm = $(dmMaker(this));
+            $dm.find("input.dmName").change(function(){
+                _dm.name = $(this).val();
+            });
+            $.each(this.options,function(){
+                $dm.find(".dmOptions").append(this.renderOption());
+                $dm.find(".addOpt").appendTo($dm.find(".dmOptions"));
+            });
+            
+            $dm.find("li.addOpt").on("click",function(){		//activate "add Option" button
+                newOpt = _conf.newOption();
+                _dm.options.push(newOpt);
+                $(this).before(newOpt.renderOption());
+            });
+            
+            return $dm;
+        };
+        
+        this.toJSON = function(){
+            return {"name":this.name,
+                    "options":$.map(_dm.options,function(opt){return opt.index})
+            };
+        };
+    
+    
+    }
+    
+    function ConflictObj(conflict){
+        var _conf = this
+        this.options = $.map(conflict.options,function(opt){
+            return new OptionObj(_conf,opt);
+        });
+         
+        this.decisionMakers = $.map(conflict.decisionMakers,function(dm){
+            return new DMObj(_conf,dm);
+        });
+        
+        this.title = conflict.title;
+        $("input.confName").change(function(){
+            _conf.title = $(this).val();
+        });
+        this.description = conflict.description;
+        $("textarea.confDesc").change(function(){
+            _conf.description = $(this).val();
+        });
+        
+        this.renderDMlist = function(){
+            $dmList = $(dmListMaker(_conf));
+            $.each(this.decisionMakers,function(){
+                $dmList.append(this.renderDM());
+            });
+            $dmList.find("li.addDM").appendTo($dmList)
+                    .on("click",function(){		        //activate "add DM" button
+                        newDM = _conf.newDecisionMaker()
+                        $(this).before(newDM.renderDM());
+                        $("ul.dmOptions").sortable({connectWith: "ul.dmOptions",
+                                            items:"> li:not(.addOpt)"});	
+                    });
+            return $dmList;
+        };
+        
+        this.renderOptionList = function(){
+            $optionList = $("<div class='optBank'>");
+            $.each(this.options,function(){
+                $optionList.append(this.renderOption());
+            });
+            return $optionList;
+        };
+            
+        this.newOption = function(){
+            var newOpt = new OptionObj(_conf);
+            $("div.optBank").append(newOpt.renderOption())
+            this.options.push(newOpt);
+            return newOpt;
+        }
+        
+        this.newDecisionMaker = function(){
+            var newDM = new DMObj(_conf);
+            this.decisionMakers.push(newDM);
+            return newDM;
+        }
+         
+        this.toJSON = function(){
+            $.each(_conf.options,function(i){this.index = i;});
 
-    $("div.dmList").html(dmListMaker(conflict));    //insert the conflict into the page
-    //fill out the options list?
+            return {"options":this.options,"decisionMakers":this.decisionMakers,
+                    "title":this.title,"description":this.description};
+        }
+    
+    };
+    
+    var conflict = new ConflictObj(conflictData);
+      
+    $("div.dmList").append(conflict.renderDMlist());    //insert the conflict into the page
+    $("div.optList").append(conflict.renderOptionList());
     $("ul.dmOptions").sortable({connectWith: "ul.dmOptions",
                                 items:"> li:not(.addOpt)"});		
     $("ul.dmList").sortable({connectWith: "ul.dmList",
@@ -42,63 +185,47 @@ var dmPageScript = function(){
 	$("div.dmList").on("sortreceive","ul.dmOptions",function(){
 		$(this).find("li.addOpt").appendTo(this);});			//keep "add Option" at end of list
 	
-	$("div.dmList").on("click","li.addOpt",function(){		//activate "add Option" button
-		$(this).before(optMaker(newOpt));
-	});
-	
-	$("div.dmList").on("click","li.addDM",function(){		//activate "add Option" button
-		$(this).before(dmMaker(newDM));
-		$("ul.dmOptions").sortable({connectWith: "ul.dmOptions",
-							items:"> li:not(.addOpt)"});	
-	});
-	
 	$("div.dmList").on("click","img.cornerX",function(){		//activate "remove" x's
-		$(this).parent().remove();
+        $(this).parent().trigger("remove").remove();
 	});
 	
 	var $iconPicker = $(Mustache.render(templates.iconChooserTemplate,iconList));
 	$iconPicker.mouseleave(function(){$iconPicker.detach()});
 	$iconPicker.find("img.iconPicker").click(function(){
 		var newSrc = $(this).attr("src");
-		$iconPicker.parent().find("img.icon").attr("src",newSrc);
+		$iconPicker.parent().find("img.icon").attr("src",newSrc).change();
 		$iconPicker.detach();
 	});
 	$("div.dmList").on("click","img.icon",function(){
 		$(this).after($iconPicker)
 	});
-	
-    var optionCollector = function(){
-        var $opt = $(this);
-        var option = {};
-        option.optName = $opt.find('input.optName').val();
-        option.rev = $opt.find('select.rev').val();
-        option.image = $opt.children('img.icon').attr('src')
-            .match(/images\/option\_icons\/([.\w]+)/)[1];
-        return option;
-    };
     
-    var dmCollector = function(){
-        var $dm = $(this);
-        var dm = {};
-        dm.name = $dm.find('input.dmName').val();
-        dm.options = $dm.find('li.option').map(optionCollector).get();
-        return dm;
+    var packConflict = function(conf){
+        packed = {};
+        packed.confTitle = conf.title;
+        packed.confDescription = conf.description;
+        packed.conf = JSON.stringify(conf);
+        return packed;
     };
-    
-    var confCollector = function(){
-        var conf = {};
-        conf.DecisionMakers = $('form.dmForm').map(dmCollector).get()
-        return conf
-    }
 
     $('#saveButton').click(function(){
-        console.log(JSON.stringify(confCollector(), '    '));
-        var conf = JSON.stringify(confCollector(), '    ')
-        $.post("", {msg:conf})
+        console.log(packConflict(conflict));
+        $.post("", packConflict(conflict))
             .done(function(data){
-                window.history.replaceState({'blah':'blah'},'a title',data);
+                if (data != "Ajax_save_success."){
+                    window.history.replaceState({'blah':'blah'},'a title',data);
+                };
                 console.log(data);
                 })
-            .fail(function(){console.log("failed");});
+            .fail(function(data){
+                console.log("failed");
+                var win=window.open('about:blank');
+                with(win.document)
+                {
+                  open();
+                  write(data.responseText);
+                  close();
+                };
+            });
     });
 };
--- gmConflicts/views.py
+++ gmConflicts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from django.core.urlresolvers import reverse
+import json
 
 from gmConflicts.models import GM_conflict
 
@@ -15,25 +16,28 @@ def index(request):
 def newConflict(request):
     if request.method == "POST":
         conf = GM_conflict(
-                title="newConf",
-                description = "A new conflict",
+                title=request.POST['confTitle'],
+                description = request.POST['confDescription'],
                 pub_date = timezone.now(),
-                json_rep = request.POST['msg'],
+                json_rep = request.POST['conf'],
                 json_stabilities = "{}")
         conf.save()
         return HttpResponse(reverse('gmConflicts:editor', args=[conf.id]))
     
     elif request.method == "GET":
-        conf = {'json_rep':"{}"}
+        conf = {'json_rep':'{"options":[],"decisionMakers":[]}',
+                'title':'New Conflict'}
         return render(request, 'gmConflicts/editor.html',{'conf':conf})
 
 @csrf_exempt
 def editor(request, conf_id):
     if request.method == "POST":
         conf = get_object_or_404(GM_conflict,pk=conf_id)
-        conf.json_rep = request.POST['msg']
+        conf.title = request.POST['confTitle']
+        conf.description = request.POST['confDescription']
+        conf.json_rep = request.POST['conf']
         conf.save()
-        return HttpResponse("Ajax save success.")
+        return HttpResponse("Ajax_save_success.")
 
     elif request.method == "GET":
         conf = get_object_or_404(GM_conflict, pk=conf_id) 
